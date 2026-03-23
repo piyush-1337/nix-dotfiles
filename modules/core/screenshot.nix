f0fe4829a87d14331ef4d5278c1cb51b7c4801b8
piyush-1337 <piyushkatkar9421@gmail.com>
2026-03-24 00:19:15 +0530

    minor fixes

--- modules/core/screenshot.nix
+++ modules/core/screenshot.nix
@@ -2,19 +2,14 @@
 
 let
   interactive-screenshot = pkgs.writeShellScriptBin "interactive-screenshot" ''
-    # 1. Prompt for the capture area
     AREA=$(echo -e "Screen\nSelection" | ${pkgs.wofi}/bin/wofi --dmenu --prompt "Capture Area:" --lines 2)
 
-    # Exit if user hits Escape
     [ -z "$AREA" ] && exit 0
 
-    # 2. Prompt for the action
     ACTION=$(echo -e "Copy\nSave\nBoth" | ${pkgs.wofi}/bin/wofi --dmenu --prompt "Action:" --lines 3)
 
-    # Exit if user hits Escape
     [ -z "$ACTION" ] && exit 0
 
-    # 3. Map selections to grimblast arguments
     GB_AREA="screen"
     [ "$AREA" = "Selection" ] && GB_AREA="area"
 
@@ -22,12 +17,11 @@ let
     [ "$ACTION" = "Save" ] && GB_ACTION="save"
     [ "$ACTION" = "Both" ] && GB_ACTION="copysave"
 
-    # 4. Execute the final command
+    sleep 0.5
     ${pkgs.grimblast}/bin/grimblast "$GB_ACTION" "$GB_AREA"
   '';
 in
 {
-  # Install the script and its dependencies
   home.packages = with pkgs; [
     interactive-screenshot
     grimblast
