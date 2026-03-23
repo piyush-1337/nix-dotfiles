4a65091b6ab37bb31e0ef8a4ee3b88ea743393ce
piyush-1337 <piyushkatkar9421@gmail.com>
2026-03-24 00:22:10 +0530

    enable gnome-keyring pam

--- configuration.nix
+++ configuration.nix
@@ -54,6 +54,7 @@
   services.upower.enable = true;
   security.pam.services.ly.enableGnomeKeyring = true;
   security.pam.services.hyprlock = { };
+  security.pam.services.login.enableGnomeKeyring = true;
 
   programs.dconf.enable = true;
 
