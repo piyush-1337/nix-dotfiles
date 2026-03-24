2eb88afd2965acf5953f06b6843fde57e59ef0be
piyush-1337 <piyushkatkar9421@gmail.com>
2026-03-24 13:25:11 +0530

    add nvidia boot driver

--- configuration.nix
+++ configuration.nix
@@ -114,6 +114,7 @@
     #   intelBusId = "PCI:0:2:0";
     #   nvidiaBusId = "PCI:1:0:0";
     # };
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # Configure network proxy if necessary
