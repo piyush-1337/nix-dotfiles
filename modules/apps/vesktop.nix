6c6491d6b8c7f6dcd6ca5165edb0dfffa5310d05
piyush-1337 <piyushkatkar9421@gmail.com>
2026-03-24 17:57:45 +0530

    dont make the vesktop config symlink(ro for the app)

--- modules/apps/vesktop.nix
+++ modules/apps/vesktop.nix
@@ -1,27 +1,7 @@
 { pkgs, ... }:
+
 {
   home.packages = [
     pkgs.vesktop
   ];
-
-  home.file.".config/vesktop/settings.json".text = ''
-    {
-      "discordBranch": "stable",
-      "arRPC": true,
-      "splashColor": "rgb(205, 214, 244)",
-      "splashBackground": "rgb(30, 30, 46)",
-      "enableReactDevtools": false,
-      "frameless": false,
-      "transparent": false,
-      "winCtrlQ": false,
-      "disableMinSize": false,
-      "winNativeTitleBar": false,
-      "plugins": {},
-      "notifications": {
-        "position": "bottom-right"
-      },
-      "hardwareAcceleration": true,
-      "electronOzonePlatformHint": "wayland"
-    }
-  '';
 }
