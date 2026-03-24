4e2c0cdff7384dd34657b75d24e1629feeee47f6
piyush-1337 <piyushkatkar9421@gmail.com>
2026-03-24 20:14:36 +0530

    switch to latest nvidia drivers instead of stable

--- configuration.nix
+++ configuration.nix
@@ -117,7 +117,7 @@
     #   intelBusId = "PCI:0:2:0";
     #   nvidiaBusId = "PCI:1:0:0";
     # };
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   # Configure network proxy if necessary
