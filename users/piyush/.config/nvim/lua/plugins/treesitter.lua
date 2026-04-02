6e0bf849a08469bf6a8984d007afa9641935f8c9
piyush-1337 <piyushkatkar9421@gmail.com>
2026-04-02 18:31:21 +0530

    remove verilog

--- users/piyush/.config/nvim/lua/plugins/treesitter.lua
+++ users/piyush/.config/nvim/lua/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return {
       'lua', 'python', 'javascript', 'typescript', 'vimdoc', 'vim', 'regex',
       'terraform', 'sql', 'dockerfile', 'toml', 'json', 'java', 'groovy',
       'go', 'gitignore', 'graphql', 'yaml', 'make', 'cmake', 'markdown',
-      'markdown_inline', 'bash', 'tsx', 'css', 'html', 'rust', 'verilog',
+      'markdown_inline', 'bash', 'tsx', 'css', 'html', 'rust',
     }
     
     require('nvim-treesitter').install(parsers)
